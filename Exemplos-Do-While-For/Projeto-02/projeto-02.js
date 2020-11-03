2585c5b9777a9dba8b2f7bf3550589dfea096966
Demarote <demarote@users.noreply.github.com>
2020-11-03 20:41:53 -0300

    Projeto para avaliação HSMU

---
+++ Exemplos-Do-While-For/Projeto-02/projeto-02.js
@@ -0,0 +1,34 @@
+var qnt_entrevistados = 0;
+var qtd_menor_10 = 0;
+var qtd_entre_10_15 = 0;
+var qtd_maior_15 = 0;
+
+function programa() {
+var pergunta = prompt("Deseja Participar da Pesquisa? 1 para SIM e 2 para NÃO");
+
+if (pergunta == 1 || pergunta == 2){
+    if (pergunta == 1){
+    var quantidade = prompt("Quantas vezes você usou o restaurante no mês");
+    qnt_entrevistados = qnt_entrevistados + 1;
+    document.getElementById("entrevistados").innerHTML = qnt_entrevistados;
+        if(quantidade < 10){qtd_menor_10 = qtd_menor_10 + 1;}
+        if(quantidade >= 10 && quantidade <= 15){qtd_entre_10_15 = qtd_entre_10_15 + 1;}
+        if(quantidade > 15){qtd_maior_15 = qtd_maior_15 + 1;}
+        var calculo1 = (100 * qtd_menor_10) / qnt_entrevistados;
+        document.getElementById("menos10").innerHTML = calculo1+'%';
+        var calculo2 = (100 * qtd_entre_10_15) / qnt_entrevistados;
+        document.getElementById("entre1015").innerHTML = calculo2+'%';
+        var calculo3 = (100 * qtd_maior_15) / qnt_entrevistados;
+        document.getElementById("mais15").innerHTML = calculo3+'%';
+        setTimeout(function(){programa();}, 1500);
+    }
+    if (pergunta == 2){
+    alert('Pena que não participou, mas obrigado mesmo assim.');
+    programa();
+    }
+}else{
+alert("Você digitou um código existente, tente novamente");
+programa();
+}
+}
+programa();
--- Exemplos-Do-While-For/for-repeticao.js
+++ Exemplos-Do-While-For/for-repeticao.js
@@ -1,5 +1,4 @@
-	
-for (var qtde_brownie = 0; qtde_brownie<10;){
+for (var qtde_brownie = 0; qtde_brownie<10; qtde_brownie++){
     alert ("Fazendo brownie...")
     }
     document.write("Cesta lotada: "+qtde_brownie)
