c5b14b79b3b14d76d2d2d61c0bc4af553368a8ef
Demarote <demarote@users.noreply.github.com>
2020-11-03 17:12:12 -0300

    Projeto para avaliação

---
+++ Projeto-02/code-school-02.js
@@ -0,0 +1,11 @@
+var qtd_brownies = 10;
+var caixa = 0;
+
+while(qtd_brownies >=1){
+
+    alert('Quantidade: '+qtd_brownies+);
+    alert('Em caixa: '+qtd_brownies+);
+    qtd_brownies = 10 - 1;
+    caixa = caixa + 3.5;
+    return;
+}
